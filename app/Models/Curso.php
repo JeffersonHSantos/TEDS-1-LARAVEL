57a5a9c9d33b0a52a00098fadc6ef7f1d3d1d09c
Jefferson Henrique Alves Dos Santos <210017@upf.br>
2026-03-18 22:14:28 -0300

    Primeiro commit

---
+++ app/Models/Curso.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    
+    // fillable é um array que define quais campos podem ser preenchidos em massa (mass assignment)
+    protected $fillable = [
+        'nome', 
+        'descricao'
+    ];
+    
+}
---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,1 @@
+<h1>teste cursos</h1>
