1a50888ebcd8b8fe1331cd4fbf32afab89622db5
Jefferson Henrique Alves Dos Santos <210017@upf.br>
2026-04-01 22:03:24 -0300

    Aula 01/04 copiado os arquivos do professor, disponibilizado pelo github, link no moodle

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -1,1 +1,14 @@
-<h1>teste cursos</h1>
+@extends('layouts.app')
+
+@section('title', 'Novo curso')
+
+@section('content')
+    <h1>Novo Curso</h1>
+
+    <form action="{{ route('cursos.store') }}" method="POST">
+        @include('cursos._form', ['buttonText' => 'Criar curso'])
+    </form>
+@endsection
+
+
+
